874d1595fad77cddae60fade93aa27cba7d0a3eb
heykiditsaaron <55932658+heykiditsaaron@users.noreply.github.com>
2024-08-22 20:24:56 -0400

    Initial setup
    All I've done is copied files generated from https://fabricmc.net/develop/template/
    This is a basic template to work from

---
+++ src/client/kotlin/com/parasites/ParasitesClient.kt
@@ -0,0 +1,9 @@
+package com.parasites
+
+import net.fabricmc.api.ClientModInitializer
+
+object ParasitesClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/com/parasites/ParasitesDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.parasites
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object ParasitesDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
